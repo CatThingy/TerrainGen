729d013dfe004f7147c6b4b084793114243a2492
Andy Chen <andyc.andychen@gmail.com>
2019-04-04 09:36:19 -0700

    Removed unnecessary variables

--- HueToRGB.js
+++ HueToRGB.js
@@ -6,13 +6,12 @@ function rgbToHue(r, g, b) {
   r /= 255, g /= 255, b /= 255;
 
   var max = Math.max(r, g, b), min = Math.min(r, g, b);
-  var h, s, l = (max + min) / 2;
+  var h = (max + min) / 2;
 
   if (max == min) {
-    h = s = 0; // achromatic
+    h = 0; // achromatic
   } else {
     var d = max - min;
-    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
 
     switch (max) {
       case r: h = (g - b) / d + (g < b ? 6 : 0); break;
